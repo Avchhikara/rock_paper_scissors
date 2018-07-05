e2f0fde608d02da337365aed68262b9a0e59bc6e
Avnish <avnishway@gmail.com>
2018-07-05 17:19:25 +0530

    adding bootstrap

--- index.php
+++ index.php
@@ -2,12 +2,47 @@
 <html>
 <head>
 	<title>Avnish</title>
+	<meta name="description" content = "The main page of the Game rock paper and scissors. From the course Building web applications in PHP taught by Dr. Charles Severance on the platform coursera.">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
 </head>
 <body>
-	<h1>Welcome to Rock Paper Scissors</h1>
-	<a href="login.php" onmouseover = "window.status = 'Login Screen';">Please Log In</a>
-	<br>
-	Attempt to go to <a href="game.php">game.php</a> without logging in - it should fail with an error message.
 
+	<nav class="navbar navbar-dark bg-dark">
+		<a href="index.php" class="navbar-brand mb-0 h1">Rock Paper Scissors</a>
+	</nav>
+
+
+	<div class="container" style="margin-top: 50px; height: 61.5vh;">
+		<div class="row">
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-5">
+				<h1>Welcome,</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12 col-sm-12 col-md-12 col-xs-12  justify-content-center">
+			<a href="login.php">Please Log In</a>
+			<br>
+			Attempt to go to <a href="game.php">game.php</a> without logging in - it should fail with an error message.
+			</div>
+		</div>
+	</div>
+	
+	<!-- Now, dealing with the footer -->
+	<div class="container-fluid bg-light footer-bottom" style="height: 22.2vh;">
+		
+		
+		<footer style="padding-top: 50px;"><center>&copy; Avnish's Projects</center></footer>
+
+	</div>
+
+
+
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
