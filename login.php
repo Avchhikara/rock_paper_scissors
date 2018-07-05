7c6143c3017a4dfc2e696dacf29f79d3e5104c56
Avnish <avnishway@gmail.com>
2018-07-05 13:47:38 +0530

    Adding styling through bootstrap

--- login.php
+++ login.php
@@ -1,18 +1,6 @@
 <?php  
 	session_start();
-	if(isset($_POST['who'])&&isset($_POST['pass'])){
-		if($_POST['pass']==='php123'){
-			$_SESSION['name'] = $_POST['who'];
-			header('Location: game.php');
-			return;
-		}
-		else{
-			echo "Incorrect Password";
-		}
-	}
-
-
-
+	unset($_SESSION['name']);
 ?>
 
 
@@ -20,19 +8,61 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+	<meta name="description" content="This is the login page of the application 'Rock-Paper-Scissors' and is used to facilite the session so, that the user can proceed further in the appication to the play area. PHP, HTML, CSS and Bootstrap are being used here!">
 	<title>Avnish's Login page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+
 </head>
 <body>
 
-	<form method="post">
-		<label for="who">User Name</label>
-		<input type="text" name="who" id="who"><br>
-		<label for="pass">Password</label>
-		<input type="Password" name="pass" id="pass"><br>
-		<input type="submit" value="Log In">
-		<input type="button" value="Cancel" onclick="location.href = 'index.php'; return false;">
-	</form>	
+	<nav class="navbar navbar-dark bg-dark">
+  		<a href="index.php" class="navbar-brand mb-0 h1">Rock Paper Scissors</a>
+	</nav>
 
 
+	<div class="container " >
+		<div class="row d-flex justify-content-center" style="margin-top:5%;">
+			<div class="col-lg-4 col-sm-7 col-md-7 col-xs-6 " >
+				<h4>Login Form</h4>
+				<form method="post" class="border" style="padding:20px;">
+					
+					<?php
+						if(isset($_POST['who'])&&isset($_POST['pass'])){
+							if($_POST['pass']==='php123'){
+								$_SESSION['name'] = $_POST['who'];
+								header('Location: game.php');
+								return;
+							}
+							else{
+								echo "<div class='alert alert-danger' role='alert'>";
+								echo "Incorrect Password";
+								echo "</div>";
+							}
+						}					 
+					?>
+					
+					<div class="form-group" >
+						<label for="who" class="h6">User Name</label>
+						<input type="text" class="form-control" name="who" id="who">
+						<small class="form-text text-muted">We don't store your name/email or any of other input</small>
+					</div>
+					<div class="form-group">
+						<label for="pass" class="h6">Password</label>
+						<input type="Password" class="form-control" name="pass" id="pass">
+						<small class="form-text ">Password: php123</small>
+					</div>
+					<input type="submit" value="Log In" class="btn btn-outline-success">
+					<input type="button" value="Cancel" onclick="location.href = 'index.php'; return false;" class="btn btn-outline-warning">
+				</form>	
+			</div>
+		</div>
+	</div>
+
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
