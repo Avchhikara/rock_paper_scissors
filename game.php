0cc1218b87b508799f043dfcd693a4ecc9609d2e
Avnish <avnishway@gmail.com>
2018-07-06 10:43:31 +0530

    Adding Bootstrap

--- game.php
+++ game.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Avnish</title>
+	<title>Game | Rock Paper Scissors</title>
 	 
 	<meta charset="UTF-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
@@ -37,7 +37,7 @@
 				<form method = "post" class="border">
 					<div class="form-group" style="padding:5vh;">
 						<select name="human" id="human" class="custom-select mr-sm-2" >
-							<option value='-1'>Select</option>
+							<option value='-1'>Select Strategy</option>
 							<option value='0'>Rock</option>
 							<option value='1'>Paper</option>
 							<option value='2'>Scissors</option>
@@ -52,7 +52,7 @@
 			</div>				
 		</div>
 		<div class="row">
-			<div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+			<div class="col-sm-6 col-xs-6 col-md-6 col-lg-12">
 
 				<pre>
 					<?php
@@ -77,7 +77,7 @@
 					$arr = array('Rock', 'Paper', 'Scissors');
 					$human = isset($_POST['human'])?$_POST['human']+0:-1;
 					if($human==-1){  
-						$output = "Please select a strategy and press Play.";
+						$output = "Please select a strategy";
 						echo "<input class='form-control' type='text' placeholder=' ".$output."' readonly>";
 					}
 
@@ -87,16 +87,26 @@
 
 							for($j=0;$j<3;$j++){
 								$out = check($arr[$i], $arr[$j]);
-								$output = "Human=".$arr[$i]." Computer=".$arr[$j]." Result=".$out."\n";
-								echo "<input class='form-control' type='text' placeholder=' ".$output." 'readonly>";
+								$output_1 = "Human=".$arr[$i];
+								$output_2=" Computer=".$arr[$j];
+								$output_3=" Result=".$out;
+								echo "<input class='form-control' type='text' placeholder=' ".$output_1." 'readonly>";
+								echo "<input class='form-control' type='text' placeholder=' ".$output_2." 'readonly>";
+								echo "<input class='form-control' type='text' placeholder=' ".$output_3." 'readonly>";
+								echo "<hr>";
 							}
 						}	
 					}
 					else{
 						$r = rand(0, 2);
 						$out = check($arr[$human], $arr[$r]);
-						$output = "Your Play=".$arr[$human]." Computer Play=".$arr[$r]." Result=".$out."\n";
-						echo "<input class='form-control' type='text' placeholder=' ".$output." 'readonly>";
+						$output_1 = "Your Play=".$arr[$human];
+						$output_2 =	" Computer Play=".$arr[$r];
+						$output_3 = " Result=".$out;
+						echo "<input class='form-control' type='text' placeholder=' ".$output_1." 'readonly>";
+						echo "<input class='form-control' type='text' placeholder=' ".$output_2." 'readonly>";
+						echo "<input class='form-control' type='text' placeholder=' ".$output_3." 'readonly>";
+						echo "<hr>";
 					}
 					?>
 				</pre>
@@ -105,7 +115,7 @@
 	</div>
 <!-- Now, dealing with the footer -->
 <div class="container-fluid bg-light footer-bottom" style="height: 22.2vh;">
-		<footer style="padding-top: 50px;"><center>&copy; Avnish's Projects</center></footer>
+	<footer style="padding-top: 50px;"><center>&copy; Avnish's Projects</center></footer>
 </div>
 </body>
 </html>
